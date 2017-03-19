7ab36bf13a0fa51743c7234ae1efe7b372e2f960
renearias <rene_811@hotmail.com>
2017-03-19 14:28:27 -0500

    image

---
+++ src/AppBundle/Form/ProfileFormType.php
@@ -0,0 +1,26 @@
+<?php
+// src/AppBundle/Form/RegistrationType.php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+         $builder->add('file');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+    }
+
+    public function getName()
+    {
+        return 'app_user_profile';
+    }
+}
