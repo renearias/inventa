66d92745d51f0cfc01e70f8a08ad19d0acbd428f
renearias <rene_811@hotmail.com>
2017-07-05 11:46:46 -0500

    descargar logs

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,15 +7,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app2/example", name="homepage2")
+     * @Route("/downloadlog", name="downloadlog")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        $file=$this->get('kernel')->getLogDir().'/'.$this->get('kernel')->getEnvironment().'.log';
+        $response = new BinaryFileResponse($file);
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT);
+        return $response;
     }
     
      /**
